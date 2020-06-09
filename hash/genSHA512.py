575e96a7957fa69b7e15bc157cb1f45478a57d2f
Nicholas Houle <nicholashoule@users.noreply.github.com>
2020-06-09 13:26:51 -0600

    Update genSHA512.py

--- hash/genSHA512.py
+++ hash/genSHA512.py
@@ -8,19 +8,28 @@ $<id>$<salt>$<hash>
 Example:
 $6$qoE2letU$wWPRl.PVczjzeMVgjiA8LLy2nOyZb...
 '''
+import sys
 import crypt
 import getpass
 
+
 # Program's Purpose
 # Multi-line comment in Python
+# This script is only compatible with crypt (C) on Linux.
 print('''Generate a sha512 password hash as produced by crypt (C)
 Please enter a password to encrypt.
 ''')
 
+# Crypt, as defined by POSIX, doesn't mandate a specific encryption algorithm 
+# hence OSX crypt() isn't compatible with this script. 
+if 'linux' not in sys.platform.lower():
+    print('This script is only compatible with crypt (C) on Linux.')
+    sys.exit(1)
+
 try:
     t1Pass, t2Pass = getpass.getpass(), getpass.getpass('Enter password again: ')
 
-    while t1Pass !=  t2Pass:
+    while t1Pass != t2Pass:
       print('Passwords do not match. Please try and enter your password again.')
       t1Pass, t2Pass = getpass.getpass(), getpass.getpass('Enter password again: ')
 
@@ -31,4 +40,3 @@ except Exception as error:
 else:
     # Print salted sha512 hash to console
     print(crypt.crypt(tPass, crypt.mksalt(crypt.METHOD_SHA512)))
-    
